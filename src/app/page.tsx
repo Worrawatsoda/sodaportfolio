c651ffd70e11c4a1d87f428162f0fbfda4042254
Worrawatsoda <Worrawatsoda@gmail.com>
2023-11-21 23:52:35 +0700

    useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
+"use client";
 import Image from 'next/image'
+import React  , { useState } from "react"
 
+// const [mySkillButton, setMySkillButton]= useState(false);
+// console.log(mySkillButton)
 export default function Home() {
+  const [mySkillButton, setMySkillButton]= useState("A");
+  console.log("ค่าตัวแปร: "+mySkillButton)
+  const handleMySkillButton = (param:any) => {
+    setMySkillButton(param)
+    console.log(param)
+    console.log("ตัวแปร: "+mySkillButton)
+  }
+  
   return (
     <main className="min-h-screen scroll-smooth">
 
-      <div className='flex fixed items-center justify-between p-[30px]  w-full h-[42px] bg-white z-40'>
-        <div className='p-[30px] cursor-pointer font-bold italic text-[28px] z-50 text-black'>WORRAWAT</div>
-        <div className='z-50 '>
+      <div className='flex fixed items-center justify-between p-[30px]  w-full h-[42px] bg-secondary '>
+        <div className='p-[30px] cursor-pointer font-bold italic text-[28px] z-50 text-textPrimary'>WORRAWAT</div>
+        <div className=' '>
           <ul className='list-none'>
             <li className='inline-block p-[20px] transition-all hover:bg-grey '>
-              <a className='transition-all hover:bg-grey text-black' href="#section1">
+              <a className='transition-all hover:bg-grey text-textPrimary' href="#section1">
                 Home
               </a>
             </li>
             <li className='inline-block p-[20px] transition-all hover:bg-grey '>
-              <a className='transition-all hover:bg-grey text-black' href="#section2">
+              <a className='transition-all hover:bg-grey text-textPrimary' href="#section2">
                 About
               </a>
             </li>
             <li className='inline-block p-[20px] transition-all hover:bg-grey '>
-              <a className='transition-all hover:bg-grey text-black' href="#section3">
+              <a className='transition-all hover:bg-grey text-textPrimary' href="#section3">
                 Section 3
               </a>
             </li>
@@ -28,18 +40,22 @@ export default function Home() {
       </div>
 
 
-      <section id="section1" className='flex items-center justify-center w-full h-[738px] bg-[#141C2C] bg-[url(/preprofile.png)] bg-cover '>
+      <section id="section1" className='flex items-center justify-center w-full h-[738px]  bg-[url(/preprofile.png)]  bg-center '>
         <h1 className='text-white text-[30px] font-extralight'>Welcome to </h1><br />
         <p className='text-white text-[30px] font-extrabold '>My Digital Portfolio</p>
 
       </section>
 
-      <section id="section2" className='flex items-center justify-center w-full h-[738px] bg-white '>
-        <p className='text-[30px]'>Introduce Youself</p>
+      <section id="section2" className='flex items-center justify-center w-full h-[738px] bg-secondary '>
+        <p className='text-[30px] '>Introduce Youself</p>
       </section>
 
-      <section id="section3" className='flex items-center justify-center w-full h-[738px] bg-[#141C2C]'>
-        <p className='text-white text-[30px]'>My skill</p>
+      <section id="section3" className='flex items-center justify-center w-full h-[738px] bg-primary'>
+        
+        <button className='text-primary w-[114px] h-[40px] bg-secondary m-[10px]' onClick={(e)=>handleMySkillButton("A")}> A </button>
+        <button className='text-primary w-[114px] h-[40px] bg-secondary m-[10px]' onClick={(e)=>handleMySkillButton("B")}> B </button>
+        {mySkillButton == "A"? (<><div className='w-[500px] h-[500px] bg-red-300'><p></p></div></>):(<></>)}
+        {mySkillButton == "B"? (<><div className='w-[50px] h-[50px] bg-red-500'></div></>):(<></>)}
       </section>
 
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+          primary: "#141C2C",
+          secondary: "#FFFFFF",
+          textPrimary: "#000000",
+          activebutton: "#FFE500",
+         }
     },
   },
   plugins: [],
 }
 export default config
+
+
+
