1085d754ab9e73b69cbb9543fa005f7670cc22ed
Worrawatsoda <Worrawatsoda@gmail.com>
2023-11-21 22:12:02 +0700

    EDIT NAVBAR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="min-h-screen scroll-smooth">
 
-      <div className='flex fixed place-items-center justify-between p-[30px]  w-full h-[42px] bg-white '>
+      <div className='flex fixed items-center justify-between p-[30px]  w-full h-[42px] bg-white '>
         <div className='p-[30px] cursor-pointer font-bold italic text-[28px]'>WORRAWAT</div>
-        <nav className=' '>
-          <ul className='list-none'>  
+        <div className=' '>
+          <ul className='list-none'>
             <li className='inline-block p-[20px] transition-all hover:bg-grey '>
               <a className='transition-all hover:bg-grey' href="#section1">
                 Home
@@ -24,22 +24,23 @@ export default function Home() {
               </a>
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
 
 
       <section id="section1" className='flex items-center justify-center w-full h-[738px] bg-[#141C2C] bg-[url(/preprofile.png)] bg-cover '>
-        <p className='text-white text-[30px]'>Add image and welcome to My Portfolio</p>
-        
+        <h1 className='text-white text-[30px] font-extralight'>Welcome to </h1><br />
+        <p className='text-white text-[30px] font-extrabold '>My Digital Portfolio</p>
+
       </section>
 
       <section id="section2" className='flex items-center justify-center w-full h-[738px] bg-white '>
         <p className='text-[30px]'>Introduce Youself</p>
-        </section>
+      </section>
 
       <section id="section3" className='flex items-center justify-center w-full h-[738px] bg-[#141C2C]'>
         <p className='text-white text-[30px]'>My skill</p>
-        </section>
+      </section>
 
 
 
